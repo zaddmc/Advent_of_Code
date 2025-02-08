5bfe970d7268827c563cae1b207c94a13a2774d2
zaddmc <davidbk927@gmail.com>
2025-02-08 16:04:43 +0100

    Day03, missing some muls

---
+++ src/days/day03.py
@@ -0,0 +1,71 @@
+"""Day3 of AoC in python"""
+
+# pylint: disable-msg=C0103
+with open("../../input/day3.txt", "r", encoding="utf-8") as data:
+    prev_letters = ""
+    comma_flag = False
+    val1 = ""
+    val2 = ""
+    product_sum = 0
+    product_sum_enb = 0
+    enabled_flag = True
+    for letter in data.read():
+        if prev_letters == "do()":
+            enabled_flag = True
+            print("Enabled")
+            prev_letters = letter
+            continue
+        if prev_letters == "don't()":
+            enabled_flag = False
+            print("Disabled")
+            prev_letters = letter
+            continue
+
+        if prev_letters[0:4] == "mul(":
+            if letter.isdigit():
+                prev_letters += letter
+                if comma_flag:
+                    val2 += letter
+                else:
+                    val1 += letter
+                continue
+            if letter == ",":
+                prev_letters += letter
+                comma_flag = True
+                continue
+            if letter == ")":
+                prev_letters += letter
+                if val2 == "":
+                    continue
+                print(f"{prev_letters}, val1 = {val1}, val2 = {val2}", end="")
+                product = int(val1) * int(val2)
+                print(f", product = {product}")
+                product_sum += product
+                if enabled_flag:
+                    product_sum_enb += product
+
+            prev_letters = ""
+            comma_flag = False
+            val1 = ""
+            val2 = ""
+            continue
+
+        prev_letters += letter
+        try:
+            if "mul(".index(prev_letters, 0, 100) == 0:
+                continue
+        except:
+            pass
+        try:
+            if "do()".index(prev_letters, 0, 100) == 0:
+                continue
+        except:
+            pass
+        try:
+            if "don't()".index(prev_letters, 0, 100) == 0:
+                continue
+        except:
+            pass
+        prev_letters = ""
+    print(product_sum)
+    print(product_sum_enb)
--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,9 +1,12 @@
+use std::fs::read_to_string;
+
 use crate::{Solution, SolutionPair};
 
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve() -> SolutionPair {
-    // Your solution here...
+    let input = read_to_string("input/day3.txt").unwrap();
+
     let sol1: u64 = 0;
     let sol2: u64 = 0;
 
