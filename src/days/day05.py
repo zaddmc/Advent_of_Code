0314c666e3110359b8891933a6ff3bbd6bfed374
zaddmc <davidbk927@gmail.com>
2025-02-09 11:32:07 +0100

    Day05, p2 succes at 4121

--- src/days/day05.py
+++ src/days/day05.py
@@ -23,26 +23,42 @@ for rule in orderrules:
         orderdict[lhs] = [rhs]
 
 
-def isValid(original):
+def isValid(original, return_fail=False):
     """no"""
     is_valid_check_list = []
     for check in original:
         if True not in [tval in is_valid_check_list for tval in orderdict[check]]:
             is_valid_check_list.append(check)
         else:
+            if return_fail:
+                return check
             return False
     return True
 
 
+def fix(original):
+    """no"""
+    work_list = []
+    for val in original:
+        for new_index in range(len(work_list) + 1):
+            work_list.insert(new_index, val)
+            if isValid(work_list):
+                break
+            work_list.remove(val)
+    if not isValid(work_list):
+        raise ValueError("Not good")
+    return int(work_list[int(len(work_list) / 2)])
+
+
 sum_val_valid = 0
+sum_val_invalid = 0
 for line in checklist:
     original_list = line[:-1].split(",")
     if isValid(original_list):
         sum_val_valid += int(original_list[int(len(original_list) / 2)])
+    else:
+        sum_val_invalid += fix(original_list)
 
-print(f"Final value {sum_val_valid}")
-print(
-    "Should be false : "
-    + str(isValid(["52", "23", "24", "83", "51", "56", "37", "48", "96", "88", "63"]))
-)
-print("Should be True  : " + str(isValid(["73", "56", "42", "27", "18", "97", "53"])))
+
+print(f"Final value for valid {sum_val_valid}")
+print(f"Final value for invalid {sum_val_invalid}")
