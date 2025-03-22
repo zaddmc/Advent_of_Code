d62ddabdbcfc377bf13b6fccb49d897b2950a26f
zaddmc <davidbk927@gmail.com>
2025-03-22 16:45:18 +0100

    day07p2, finished with 162042343638683

--- src/days/day07.py
+++ src/days/day07.py
@@ -8,10 +8,9 @@ with open("../../input/day7.txt", "r") as file:
         DATA.append((int(result), new_vals))
 
 
-def step(pre_val, remaining_vals, result, cool=False):
+def step(pre_val, remaining_vals, result):
     add = pre_val + remaining_vals[0]
     mult = pre_val * remaining_vals[0]
-    con = int(str(pre_val) + str(remaining_vals[0]))
 
     if len(remaining_vals) == 1:
         return result in (add, mult)
@@ -21,10 +20,29 @@ def step(pre_val, remaining_vals, result, cool=False):
     )
 
 
+def stepcat(pre_val: int, remaining_vals: list[int], result: int) -> bool:
+    add = pre_val + remaining_vals[0]
+    mult = pre_val * remaining_vals[0]
+    con = int(str(pre_val) + str(remaining_vals[0]))
+
+    if len(remaining_vals) == 1:
+        return result in (add, mult, con)
+
+    return (
+        stepcat(add, remaining_vals[1:], result)
+        | stepcat(mult, remaining_vals[1:], result)
+        | stepcat(con, remaining_vals[1:], result)
+    )
+
+
 p1 = 0
 p2 = 0
 for result, vals in DATA:
     if step(vals[0], vals[1:], result):
         p1 += result
 
+    if stepcat(vals[0], vals[1:], result):
+        p2 += result
+
 print(p1)
+print(p2)
