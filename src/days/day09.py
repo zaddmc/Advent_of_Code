33c0d5936de1bf243f8f7e62c9beab9ce21ab2b2
zaddmc <davidbk927@gmail.com>
2025-03-23 11:28:08 +0100

    day09, Finished at 6200294120911
    due to id's being split as a string but not as a list

--- src/days/day09.py
+++ src/days/day09.py
@@ -3,18 +3,18 @@ with open("../../input/day9.txt", "r") as file:
     DATA = file.read()[:-1]
 
 
-def expand(string: str) -> str:
+def expand(string: str) -> list[str]:
     cur_id: int = 0
     state: bool = True  # when true will use id
-    new_str: str = ""
+    new_str: list[str] = []
     for char in string:
         if state:
-            new_str += str(cur_id) * int(char)
+            new_str.extend([str(cur_id) for _ in range(int(char))])
 
             state = False
             cur_id += 1
         else:
-            new_str += "." * int(char)
+            new_str.extend(["." for _ in range(int(char))])
 
             state = True
     return new_str
@@ -26,22 +26,22 @@ def get_last_char(string: str, minus_i: int) -> int:
     return minus_i
 
 
-def collapse(string: str) -> str:
-    new_str: str = ""
+def collapse(string: list[str]) -> str:
+    new_str: list[str] = []
     minus_i = -1
-    for i in range(len(string) - string.count(".", 0, len(string))):
+    for i in range(len(string) - string.count(".")):
         if string[i] == ".":
             minus_i = get_last_char(string, minus_i)
-            new_str += string[minus_i]
+            new_str.append(string[minus_i])
             minus_i -= 1
         else:
-            new_str += string[i]
+            new_str.append(string[i])
 
-    new_str += "." * (len(string) - len(new_str))
+    new_str.extend(["." for _ in range(len(string) - len(new_str))])
     return new_str
 
 
-def checksum(string: int) -> int:
+def checksum(string: list[str]) -> int:
     sum = 0
     for i, char in enumerate(string):
         if char == ".":
@@ -51,6 +51,5 @@ def checksum(string: int) -> int:
     return sum
 
 
-print(collapse(expand(DATA)))
 p1 = checksum(collapse(expand(DATA)))
 print(f"{p1=}")
