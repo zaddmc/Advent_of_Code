8742193f289a52a75094a6a275ef5a124e8c88dc
zaddmc <davidbk927@gmail.com>
2025-03-23 22:57:55 +0100

    day09p2, not good, at 6227239808683

--- src/days/day09.py
+++ src/days/day09.py
@@ -3,20 +3,22 @@ with open("../../input/day9.txt", "r") as file:
     DATA = file.read()[:-1]
 
 
-def expand(string: str) -> list[str]:
+def expand(string: str, sectionize: bool) -> list[str]:
     cur_id: int = 0
     state: bool = True  # when true will use id
-    new_str: list[str] = []
+    new_str: list = []
+    appender = new_str.append if sectionize else new_str.extend
     for char in string:
         if state:
-            new_str.extend([str(cur_id) for _ in range(int(char))])
+            appender([str(cur_id) for _ in range(int(char))])
 
             state = False
             cur_id += 1
         else:
-            new_str.extend(["." for _ in range(int(char))])
+            appender(["." for _ in range(int(char))])
 
             state = True
+
     return new_str
 
 
@@ -26,7 +28,7 @@ def get_last_char(string: str, minus_i: int) -> int:
     return minus_i
 
 
-def collapse(string: list[str]) -> str:
+def collapse(string: list[str]) -> list[str]:
     new_str: list[str] = []
     minus_i = -1
     for i in range(len(string) - string.count(".")):
@@ -41,15 +43,47 @@ def collapse(string: list[str]) -> str:
     return new_str
 
 
+def reorder(string: list[list[str]]):
+    for i_sec, section in enumerate(string.copy()[::-1]):
+        if "." in section or section == []:
+            continue
+        for i_spot, spot in enumerate(string):
+            if "." not in spot:
+                continue
+            if len(string) - (i_sec - 2) <= i_spot:
+                break
+            if len(spot) >= len(section):
+                string[string.index(section, 0, len(string))] = [
+                    "." for _ in range(len(section))
+                ]
+                string[i_spot] = [char for char in section]
+                string.insert(
+                    i_spot + 1, ["." for _ in range(len(spot) - len(section))]
+                )
+
+                break
+    return string
+
+
+def simplify(string: list[list]) -> list:
+    new_list: list = []
+    for sub_list in string:
+        new_list.extend(sub_list)
+    return new_list
+
+
 def checksum(string: list[str]) -> int:
     sum = 0
     for i, char in enumerate(string):
         if char == ".":
-            break
+            continue
         sum += i * int(char)
-
     return sum
 
 
-p1 = checksum(collapse(expand(DATA)))
+p1 = checksum(collapse(expand(DATA, False)))
 print(f"{p1=}")
+
+print(checksum(simplify(reorder(expand("2333133121414131402", True)))))
+p2 = checksum(simplify(reorder(expand(DATA, True))))
+print(f"{p2=}")
