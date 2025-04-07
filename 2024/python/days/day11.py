e569bfd8511306164194b52531e7691ba01959ee
zaddmc <davidbk927@gmail.com>
2025-04-07 22:00:53 +0200

    day11p2, finished at 225404711855335
    Switched from lists to counter objects as the list solution got at sigterm for memory leaking and taking way too long

--- 2024/python/days/day11.py
+++ 2024/python/days/day11.py
@@ -1,11 +1,17 @@
+from collections import Counter
+
+
 def solve() -> tuple[int, int]:
     get_data()
 
-    stones = map(int, DATA.split())
-    for _ in range(25):
-        stones = blink(stones)
+    stones = Counter(map(int, DATA.split()))
+    stones = blink_n(stones, 25)
+    b_25 = count_stones(stones)
 
-    return (len(stones), -1)
+    stones = blink_n(stones, 50)
+    b_75 = count_stones(stones)
+
+    return (b_25, b_75)
 
 
 def get_data(test_data: bool = False) -> str:
@@ -18,23 +24,32 @@ def get_data(test_data: bool = False) -> str:
     return DATA
 
 
-def blink(stones: list[int]):
-    new_stones: list[int] = []
-    for elm in stones:
-        if elm == 0:
-            new_stones.append(1)
+def blink(stones: Counter):
+    new_stones = Counter()
+    for val, qty in stones.items():
+        if val == 0:
+            new_stones[1] += qty
             continue
 
-        elm_str = str(elm)
+        elm_str = str(val)
         elm_len = len(elm_str)
         if elm_len % 2 == 0:
-            new_stones.append(int(elm_str[: elm_len // 2]))
-            new_stones.append(int(elm_str[elm_len // 2 :]))
+            new_stones[int(elm_str[: elm_len // 2])] += qty
+            new_stones[int(elm_str[elm_len // 2 :])] += qty
             continue
-
-        new_stones.append(elm * 2024)
+        new_stones[val * 2024] += qty
 
     return new_stones
+
+
+def blink_n(stones: Counter, n: int) -> Counter:
+    for _ in range(n):
+        stones = blink(stones)
+    return stones
+
+
+def count_stones(stones: Counter) -> int:
+    return sum(stones.values())
 
 
 if __name__ == "__main__":
