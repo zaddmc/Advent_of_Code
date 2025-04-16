5d11e0d5bc8d75f79b10b35349d51cf41f349d3a
zaddmc <davidbk927@gmail.com>
2025-04-16 23:31:45 +0200

    day13, better format
    how the fuck am i meant to solve it, i guess its with mod but how

--- 2024/python/days/day13.py
+++ 2024/python/days/day13.py
@@ -1,30 +1,31 @@
 def solve() -> tuple[int, int]:
     get_data()
-    tokens = 0
-    for _ in range(len(DATA) // 4 + 1):
-        tokens += claw_machine()
-    return (tokens, -1)
+    p1 = sum(claw_machine(segment, False) for segment in get_segments())
+    p2 = sum(claw_machine(segment, True) for segment in get_segments())
+    return (p1, p2)
 
 
-def claw_machine() -> int:
-    segment = next_segment()
+def claw_machine(segment, ispart2: bool) -> int:
     claw_A = disect(segment[0])
     claw_B = disect(segment[1])
     goal = disect(segment[2])
 
-    return brute_force(claw_A, claw_B, goal)
+    if ispart2:
+        goal = (goal[0] + 10000000000000, goal[1] + 10000000000000)
+
+    # return min(modding(claw_A, claw_B, goal), default=0)
+    return min(brute_force(claw_A, claw_B, goal), default=0)
 
 
 def brute_force(claw_A, claw_B, goal) -> int:
-    possible = []
     for a in range(100):
         for b in range(100):
             if tup_add(tup_mul(claw_A, a), tup_mul(claw_B, b)) == goal:
-                possible.append(a * 3 + b)
-    if len(possible) == 0:
-        return 0
-    possible.sort()
-    return possible[0]
+                yield a * 3 + b
+
+
+def modding(claw_A, claw_B, goal) -> int:
+    yield 0
 
 
 def disect(seg: str) -> tuple[int, int]:
@@ -41,13 +42,9 @@ def tup_add(tup1: tuple[int, int], tup2: tuple[int, int]) -> tuple[int, int]:
     return (tup1[0] + tup2[0], tup1[1] + tup2[1])
 
 
-segment_progress = -1
-
-
-def next_segment():
-    global segment_progress
-    segment_progress += 1
-    return DATA[segment_progress * 4 : segment_progress * 4 + 3]
+def get_segments():
+    for prog in range(len(DATA) // 4 + 1):
+        yield DATA[prog * 4 : prog * 4 + 3]
 
 
 def get_data():
