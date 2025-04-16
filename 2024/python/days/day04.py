86c19917a00f7ae479aebe77d0e34aeeec9d1d29
zaddmc <davidbk927@gmail.com>
2025-04-17 00:25:07 +0200

    re structuring of day04

--- 2024/python/days/day04.py
+++ 2024/python/days/day04.py
@@ -1,14 +1,29 @@
-"""No"""
-
-# pylint: disable-msg=C0103
-
-DATA = []
-with open("../../input/day4.txt", "r", encoding="utf-8") as source:
-    for line in source.readlines():
-        DATA.append(line[0:-1])
+def solve() -> tuple[int, int]:
+    get_data()
+    p1 = part1()
+    p2 = part2()
+    return (p1, p2)
 
 
-def check(x, y) -> int:
+def get_data():
+    global DATA
+
+    DATA = []
+    with open("../../input/day04.txt", "r", encoding="utf-8") as source:
+        for line in source.readlines():
+            DATA.append(line[0:-1])
+
+
+def part1():
+    count = 0
+    for xdx, line in enumerate(DATA):
+        for ydx, letter in enumerate(line):
+            if letter == "X":
+                count += check_part1(xdx, ydx)
+    return count
+
+
+def check_part1(x, y) -> int:
     """no"""
     succes = 0
     directions = [
@@ -38,9 +53,40 @@ def check(x, y) -> int:
     return succes
 
 
-count = 0
-for xdx, line in enumerate(DATA):
-    for ydx, letter in enumerate(line):
-        if letter == "X":
-            count += check(xdx, ydx)
-print(count)
+def part2():
+    count = 0
+    for xdx, line in enumerate(DATA):
+        for ydx, letter in enumerate(line):
+            if letter == "A":
+                count += check_part2(xdx, ydx)
+    return count
+
+
+def check_part2(x, y) -> int:
+    """no"""
+    if x >= len(DATA) - 1 or x < 1:
+        return 0
+    if y >= len(DATA) - 1 or y < 1:
+        return 0
+    vals = [
+        DATA[x + 1][y + 1],  # down-right
+        DATA[x + 1][y - 1],  # down-left
+        DATA[x - 1][y - 1],  # up-left
+        DATA[x - 1][y + 1],  # up-right
+    ]
+    # Filter out invalid entries
+    if "X" in vals or "A" in vals:
+        return 0
+    # Find Valid postionings
+    for rot in range(4):
+        if (
+            vals[0 + rot] == vals[(1 + rot) % 4]
+            and vals[(1 + rot) % 4] != vals[(2 + rot) % 4]
+            and vals[(2 + rot) % 4] == vals[(3 + rot) % 4]
+        ):
+            return 1
+    return 0
+
+
+if __name__ == "__main__":
+    print(solve())
