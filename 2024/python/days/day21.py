ff21fc959740ea8f48fefef9d60663b4bf60ae2b
zaddmc <davidbk927@gmail.com>
2025-04-16 20:59:17 +0200

    day21, finished at 206798
    day21p2, finished at 251508572750680

--- 2024/python/days/day21.py
+++ 2024/python/days/day21.py
@@ -1,109 +1,51 @@
 from functools import cache
+from itertools import pairwise
 
 
 def solve() -> tuple[int, int]:
-    global ndict
-    get_data(True)
-    ndict = {}
-    generate_dict(False)
-    generate_dict(True)
-
-    return (part1(), -1)
+    get_data(False)
+    p1 = start(2)
+    p2 = start(25)
+    return (p1, p2)
 
 
-def part1():
-    length = 0
-    goal = [
-        "<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A",
-        "<v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A",
-        "<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A",
-        "<v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A",
-        "<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A",
-    ]
-    for code, goa in zip(DATA, goal):
-        print(code)
-        step = convert(code)
-        print(step)
-
-        step = convert(step)
-        print(step)
-        step = convert(step)
-
-        length += len(step) * int(code[:-1])
-        print("mine", step)
-        print("goal", goa)
-        print()
-    return length
+NUMERIC = "789" "456" "123" " 0A"
+ARROWS = " ^A" "<v>"
 
 
-def convert(code: str) -> str:
-    mcode = "A" + code
-    replacement = ""
-    for idx in range(len(code)):
-        replacement += ndict[mcode[idx] + mcode[idx + 1]]
-    return replacement
-
-
-def translate(start: str, end: str, isnum: bool) -> str:
-    start_pos = get_index(start, isnum)
-    end_pos = get_index(end, isnum)
-
-    vertical = end_pos[0] - start_pos[0]
-    horizontal = end_pos[1] - start_pos[1]
-    result = ""
-
-    if isnum and start_pos[0] == 0:
-        result += tran_hor(horizontal)
-        result += tran_ver(vertical)
-    elif isnum and start_pos[1] == 3:
-        result += tran_ver(vertical)
-        result += tran_hor(horizontal)
-    elif start == "<":
-        result += tran_hor(horizontal)
-        result += tran_ver(vertical)
-    elif end == "<":
-        result += tran_ver(vertical)
-        result += tran_hor(horizontal)
-    else:
-        result += tran_hor(horizontal)
-        result += tran_ver(vertical)
-
-    result += "A"
-    return result
-
-
-def tran_ver(val):
-    if val < 0:
-        return "^" * (val * -1)
-    else:
-        return "v" * val
-
-
-def tran_hor(val):
-    if val < 0:
-        return "<" * (val * -1)
-    else:
-        return ">" * val
-
-
-def generate_dict(isnum: bool) -> dict:
-    global ndict
-    chars = "A0123456789" if isnum else "A^<v>"
-    for start in chars:
-        for end in chars:
-            res = translate(start, end, isnum)
-            ndict[start + end] = res
-    return ndict
+def start(steps: int) -> int:
+    return sum(cost(NUMERIC, code, steps + 1) * int(code[:-1]) for code in DATA)
 
 
 @cache
-def get_index(char: str, isnum: bool) -> tuple[int, int]:
-    layout = ["789", "456", "123", " 0A"] if isnum else [" ^A", "<v>"]
-    for idx, row in enumerate(layout):
-        idy = row.find(char, 0, 3)
-        if idy != -1:
-            return (idx, idy)
-    raise Exception("Char not found")
+def cost(keypad: str, path: str, steps: int) -> int:
+    return sum(step_cost(keypad, a, b, steps) for a, b in pairwise("A" + path))
+
+
+@cache
+def step_cost(keypad: str, start: str, end: str, steps: int) -> int:
+    return (
+        min(cost(ARROWS, path, steps - 1) for path in calc_path(keypad, start, end))
+        if steps
+        else 1
+    )
+
+
+def calc_path(keypad: str, start: str, end: str):
+    y1, x1 = divmod(keypad.index(start), 3)
+    y2, x2 = divmod(keypad.index(end), 3)
+    hor = "<>"[x2 > x1] * abs(x2 - x1)
+    ver = "^v"[y2 > y1] * abs(y2 - y1)
+    for path in {hor + ver, ver + hor}:
+        if " " not in walk(keypad, x1, y1, path):
+            yield path + "A"
+
+
+def walk(keypad, x, y, path):
+    i = y * 3 + x
+    for direction in path:
+        i += (-1, 1, -3, 3)["<>^v".index(direction)]
+        yield keypad[i]
 
 
 def get_data(test: bool = False):
