2dd8297f4b570b120d71cca871627f283e425c66
zaddmc <davidbk927@gmail.com>
2025-05-02 23:53:01 +0200

    day24, fuuuuuuuck
    also uploaded example data

--- 2024/python/days/day24.py
+++ 2024/python/days/day24.py
@@ -4,7 +4,33 @@ from queue import Queue
 def solve() -> tuple[int, int]:
     get_data(False)
     p1 = run()
+    swap_wires()
+    print(bin(p1))
     return (p1, -1)
+
+
+def swap_wires():
+    x_bits = ""
+    y_bits = ""
+    for key, val in map(lambda e: e.split(), INITIAL):
+        if key[0] == "x":
+            x_bits += val
+        elif key[0] == "y":
+            y_bits += val
+    x_bits = x_bits[::-1]
+    print("x:", x_bits)
+    y_bits = y_bits[::-1]
+    print("y:", y_bits)
+
+    bit_goal = int(x_bits, 2) + int(y_bits, 2)
+    print(bin(bit_goal))
+
+    for lkey, opt, rkey, dest in map(
+        lambda e: e.split(), map(lambda e: e.replace("-> ", ""), OPERATIONS)
+    ):
+        if lkey[0] == "x" and rkey[0] == "y" and opt == "AND":
+            # print(lkey, opt, rkey, dest)
+            pass
 
 
 def run():
@@ -16,19 +42,17 @@ def run():
     for oper in OPERATIONS:
         operations.put(oper.replace("-> ", ""))
 
+    operators = {
+        "OR": lambda x, y: x | y,
+        "AND": lambda x, y: x & y,
+        "XOR": lambda x, y: x ^ y,
+    }
+
     while not operations.empty():
         lkey, opt, rkey, dest = operations.get().split()
 
         if lkey in memory.keys() and rkey in memory.keys():
-            match opt:
-                case "AND":
-                    memory[dest] = memory[lkey] and memory[rkey]
-                case "OR":
-                    memory[dest] = memory[lkey] or memory[rkey]
-                case "XOR":
-                    memory[dest] = memory[lkey] ^ memory[rkey]
-                case _:
-                    raise Exception("Unkown Operator")
+            memory[dest] = operators[opt](memory[lkey], memory[rkey])
             continue
 
         operations.put(f"{lkey} {opt} {rkey} {dest}")
