a333fe8d0647d5420006d425e99698a8df8e11b4
zaddmc <davidbk927@gmail.com>
2025-05-05 22:45:07 +0200

    day22p2, finished at 2223
    used brute force and assumedthe values would be between -3 and 4, which turned out great, only taking 10 minutes

--- 2024/python/days/day22.py
+++ 2024/python/days/day22.py
@@ -3,17 +3,44 @@ def solve() -> tuple[int, int]:
 
     values = list(find_all())
     p1 = sum(list(map(lambda lis: lis[-1], values)))
-    print(list(find_change(values)))
-    return (p1, -1)
+    changes = list(find_change(values))
+    # p2 = sum([result_series(change, [-2, 1, -1, 3]) for change in changes])
+
+    p2 = max(list(brute_force(changes)))
+    return (p1, p2)
+
+
+def brute_force(changes: list[list]):
+    low, high = -3, 4
+    for x in range(low, high):
+        for y in range(low, high):
+            for z in range(low, high):
+                for w in range(low, high):
+                    yield sum(
+                        [result_series(change, [x, y, z, w]) for change in changes]
+                    )
+
+
+def result_series(serie: list[int], target: list[int]):
+    previous = serie[:4]
+    banana = sum(previous)
+    for val in serie[4:]:
+        previous.pop(0)
+        banana += val
+        previous.append(val)
+
+        if previous == target:
+            return banana
+    return 0
 
 
 def find_change(values):
     for lis in values:
-        pre_val = lis[0]
-        changes = []
+        pre_val = lis[0] % 10
+        changes = [pre_val]
         for val in lis[1:]:
-            changes.append(val - pre_val)
-            pre_val = val
+            changes.append(val % 10 - pre_val)
+            pre_val = val % 10
         yield changes
 
 
@@ -43,7 +70,7 @@ def prune(num: int) -> int:
 def get_data(test: bool):
     global DATA
     if test:
-        DATA = [1, 10, 100, 2024]
+        DATA = [1, 2, 3, 2024]
     else:
         with open("../../input/day22.txt", "r") as file:
             DATA = map(int, file.read().splitlines())
