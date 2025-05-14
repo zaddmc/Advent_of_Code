e83c69d27dc53feefceebd3e9a99981cd80f240e
zaddmc <davidbk927@gmail.com>
2025-05-14 11:24:21 +0200

    day06, finished

--- 2015/python/days/day06.py
+++ 2015/python/days/day06.py
@@ -1,33 +1,58 @@
+import numpy as np
+
+
 def solve() -> tuple[int, int]:
     get_data()
     p1 = interpret()
-    return (p1, -1)
+    p2 = interpret_elvish()
+    return (p1, p2)
 
 
 def interpret():
-    actions = {
-        "toggle": lambda b: not b,
-        "on": lambda _: True,
-        "off": lambda _: False,
-    }
-    for action, frm, _, to in map(lambda s: s.split(" "), INSTRUCTIONS):
-        for line in range(*map(int, frm.split(","))):
-            for val in range(*map(int, to.split(","))):
-                GRID[line][val] = actions[action](GRID[line][val])
+    grid = np.zeros((1000, 1000))
+    for action, frm, _, to in INSTRUCTIONS:
+        x0, y0 = map(int, frm.split(","))
+        x1, y1 = map(int, to.split(","))
 
-    count = 0
-    for line in GRID:
-        for val in line:
-            count += val
-    return count
+        match action:
+            case "on":
+                grid[x0 : x1 + 1, y0 : y1 + 1] = 1
+            case "off":
+                grid[x0 : x1 + 1, y0 : y1 + 1] = 0
+            case "toggle":
+                grid[x0 : x1 + 1, y0 : y1 + 1] = 1 - grid[x0 : x1 + 1, y0 : y1 + 1]
+
+    return int(grid.sum())
+
+
+def interpret_elvish():
+    grid = np.zeros((1000, 1000))
+    for action, frm, _, to in INSTRUCTIONS:
+        x0, y0 = map(int, frm.split(","))
+        x1, y1 = map(int, to.split(","))
+
+        match action:
+            case "on":
+                grid[x0 : x1 + 1, y0 : y1 + 1] += 1
+            case "off":
+                grid[x0 : x1 + 1, y0 : y1 + 1] = np.maximum(
+                    0, grid[x0 : x1 + 1, y0 : y1 + 1] - 1
+                )
+            case "toggle":
+                grid[x0 : x1 + 1, y0 : y1 + 1] += 2
+
+    for row in grid:
+        for col in row:
+            if col < 0:
+                print(col)
+    return int(grid.sum())
 
 
 def get_data():
-    global INSTRUCTIONS, GRID
+    global INSTRUCTIONS
     with open("../../input/day06.txt", "r") as file:
         INSTRUCTIONS = file.read().splitlines()
-    INSTRUCTIONS = map(lambda s: s.replace("turn ", ""), INSTRUCTIONS)
-    GRID = [[False for _ in range(1000)] for _ in range(1000)]
+    INSTRUCTIONS = list(map(lambda s: s.replace("turn ", "").split(" "), INSTRUCTIONS))
 
 
 if __name__ == "__main__":
