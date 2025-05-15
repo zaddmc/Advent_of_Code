f58a7ffa9e596d3072e342939d79e925dc41fe4f
zaddmc <davidbk927@gmail.com>
2025-05-15 11:51:19 +0200

    day10, accidently overwritten

--- 2015/python/days/day10.py
+++ 2015/python/days/day10.py
@@ -1,22 +1,36 @@
 def solve() -> tuple[int, int]:
     get_data()
-    print(validate("ghijklmn"))
-    print(validate("g"))
-    return (-1, -1)
+    string = DATA
+    for _ in range(40):
+        string = expand(string)
+    p1 = len(string)
+    for _ in range(10):
+        string = expand(string)
+    p2 = len(string)
+    return (p1, p2)
 
 
-def validate(string):
-    for char in string:
-        if char in "iol":
-            return False
+def expand(string):
+    previous_char = string[0]
+    count = 1
+    new_string = ""
+    for new_char in string[1:]:
+        if new_char == previous_char:
+            count += 1
+        else:
+            new_string += str(count) + previous_char
+            previous_char = new_char
+            count = 1
+    else:
+        new_string += str(count) + previous_char
 
-    return True
+    return new_string
 
 
 def get_data():
     global DATA
     with open("../../input/day10.txt", "r") as file:
-        DATA = file.read().splitlines()
+        DATA = file.read().strip()
 
 
 if __name__ == "__main__":
