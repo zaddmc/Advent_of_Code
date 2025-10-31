19a2e9e65c3c71382b3456808962932182e963af
zaddmc <davidbk927@gmail.com>
2025-10-31 10:28:51 +0100

    hash is way too fucking hard

--- 2017/day10/part2.py
+++ 2017/day10/part2.py
@@ -1,29 +1,35 @@
 def solve(data, length):
+
     values = list(range(length))
 
     ptr = 0
     skip = 0
-    for change in data:
-        part = []
-        for offset in range(change):
-            part.append(values[(ptr + offset) % length])
-        part.reverse()
-        for offset, val in enumerate(part):
-            values[(ptr + offset) % length] = val
+    for _ in range(64):
+        for change in data:
+            part = []
+            for offset in range(change):
+                part.append(values[(ptr + offset) % length])
+            part.reverse()
+            for offset, val in enumerate(part):
+                values[(ptr + offset) % length] = val
 
-        ptr += change + skip
-        skip += 1
+            ptr += change + skip
+            skip += 1
 
-    return values[0] * values[1]
+    return values
 
 
 if __name__ == "__main__":
-    # assert solve([3, 4, 1, 5], 5) == 12
+    suffix = b"\x11\x1f\x49\x2f\x17"
+
+    assert solve(b"3,4,1,5" + suffix, 5) == 12
 
     with open("input.txt", "r") as file:
         data = file.read().strip()
-    data += ",17,31,73,47,23"
-    a = bytes(data, "utf-8") + 
-    for v in a:
-        print(v, end=",")
+    start = "1,2,3"
+    suffix = b"\x11\x1f\x49\x2f\x17"
+    asd = start.encode("utf-8") + suffix
+    for b in asd:
+        print(b, end=", ")
+
     # print(solve(data, 256))
