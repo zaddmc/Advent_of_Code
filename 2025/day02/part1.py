9069ab2af1ebbec8f09ee0d8af6d8b13ddb9a19b
zaddmc <davidbk927@gmail.com>
2025-12-02 12:37:35 +0100

    i was way faster than william

---
+++ 2025/day02/part1.py
@@ -0,0 +1,27 @@
+def solve(ranges):
+    ranges = ranges.split(",")
+    val = 0
+    for ran in ranges:
+        val += sum(does_repeat(ran))
+    return val
+
+
+def does_repeat(value):
+    lhs, rhs = value.split("-")
+    for val in range(int(lhs), int(rhs) + 1):
+        val = str(val)
+        if len(val) % 2 == 1:
+            continue
+
+        slen = len(val) // 2
+        if val[:slen] == val[slen:]:
+            yield int(val)
+
+
+if __name__ == "__main__":
+    test_val = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+    assert solve(test_val) == 1227775554
+
+    with open("input.txt", "r") as file:
+        DATA = file.read().strip()
+    print(solve(DATA))
