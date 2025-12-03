e4913cda2594d86aa62972d00b4fb024d6c12da2
zaddmc <davidbk927@gmail.com>
2025-12-03 16:10:43 +0100

    i am now content

--- 2025/day03/part2.py
+++ 2025/day03/part2.py
@@ -1,15 +1,17 @@
 def solve(data):
     value = 0
-
     for line in data:
-        line = [int(i) for i in line]
-        value += find_val(line)
-
+        value += int(find_max(line, 12))
     return value
 
 
-def find_val(arr):
-    return 1
+def find_max(bank: str, length: int):
+    if length == 0:
+        return ""
+    next_digit: str = max(bank[: len(bank) - length + 1])
+    next_digit_pos: int = bank.find(next_digit)
+    return next_digit + find_max(bank[next_digit_pos + 1 :], length - 1)
+    return next_digit + max_joltage(bank[next_digit_pos + 1 :], length - 1)
 
 
 if __name__ == "__main__":
