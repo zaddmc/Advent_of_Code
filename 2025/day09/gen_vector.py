afed79796931af7516d1f3fc2e408dbd6d69d93c
zaddmc <davidbk927@gmail.com>
2025-12-09 23:11:46 +0100

    vector3

--- 2025/day09/gen_vector.py
+++ 2025/day09/gen_vector.py
@@ -4,7 +4,7 @@ from itertools import pairwise
 def generate(points):
     points = [tuple(map(int, t.split(","))) for t in points]
     for p1, p2 in pairwise(points):
-        yield f'<line x1="{p1[0]}" y1="{p1[1]}" x2="{p2[0]}" y2="{p2[1]}" style="stroke:red;stroke-width:5"/>\n'
+        yield f'<line x1="{p1[0]}" y1="{p1[1]}" x2="{p2[0]}" y2="{p2[1]}" style="stroke:red;stroke-width:10"/>\n'
 
 
 if __name__ == "__main__":
