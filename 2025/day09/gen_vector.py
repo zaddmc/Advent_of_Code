aceab6bc584844520b263f43c0db29593f995d35
zaddmc <davidbk927@gmail.com>
2025-12-09 23:07:42 +0100

    vector2

--- 2025/day09/gen_vector.py
+++ 2025/day09/gen_vector.py
@@ -4,11 +4,15 @@ from itertools import pairwise
 def generate(points):
     points = [tuple(map(int, t.split(","))) for t in points]
     for p1, p2 in pairwise(points):
-        string = f'<line x1="{p1[0]}" y1="{p2[0]}" x2="{p1[1]}" y2="{p2[1]}" style="stroke:black;stroke-width:5" />'
-        print(string)
+        yield f'<line x1="{p1[0]}" y1="{p1[1]}" x2="{p2[0]}" y2="{p2[1]}" style="stroke:red;stroke-width:5"/>\n'
 
 
 if __name__ == "__main__":
     with open("input.txt", "r") as file:
         DATA = file.read().strip().splitlines()
     res = generate(DATA)
+
+    with open("input.svg", "w") as file:
+        file.write('<svg height="100000" width="100000">\n')
+        file.writelines(res)
+        file.write("</svg>\n")
