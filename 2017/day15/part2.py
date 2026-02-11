976c4ddb868d87ec564eb8424222f30f3de72891
zaddmc <davidbk927@gmail.com>
2026-02-11 12:41:04 +0100

    compared the bin value incorrectly

--- 2017/day15/part2.py
+++ 2017/day15/part2.py
@@ -2,11 +2,11 @@ def solve(genA, genB):
     genA_factor = 16807
     genB_factor = 48271
 
-    Agen = gen(genA_factor, genA)
-    Bgen = gen(genB_factor, genB)
+    Agen = gen(genA_factor, genA, 4)
+    Bgen = gen(genB_factor, genB, 8)
 
     pair_count = 0
-    for itt in range(40_000_000):
+    for itt in range(5_000_000):
         a_val = next(Agen)
         b_val = next(Bgen)
 
@@ -16,15 +16,16 @@ def solve(genA, genB):
     return pair_count
 
 
-def gen(factor: int, start_val: int):
+def gen(factor: int, start_val: int, mult: int):
     val = start_val
     while True:
         val = val * factor % 2147483647
-        yield val
+        if val % mult == 0:
+            yield val
 
 
 if __name__ == "__main__":
-    assert solve(65, 8921) == 588
+    assert solve(65, 8921) == 309
 
     with open("input.txt", "r") as file:
         DATA = file.read().strip().splitlines()
